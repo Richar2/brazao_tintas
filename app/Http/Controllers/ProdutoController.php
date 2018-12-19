5e1073bbf85bd6ee8a3efe86e8fe3bab5d031d6d
Richar2 <richard.coimec91@hotmail.com>
2018-12-19 14:43:42 -0200

    test2

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -37,12 +37,7 @@ class ProdutoController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         //
