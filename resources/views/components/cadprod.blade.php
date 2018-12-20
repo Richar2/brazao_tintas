86dc0fafc90f76ec8ba273efe80d4bf9984fbd62
Richar2 <richard.coimec91@hotmail.com>
2018-12-19 23:24:30 -0200

    Atualização view

--- resources/views/components/cadprod.blade.php
+++ resources/views/components/cadprod.blade.php
@@ -13,9 +13,31 @@
        {!!Form::open() !!}
              {{ csrf_field() }}
              <label>
+               {!!Form::text('nome_pro',null,['class'=>'input', 'placeholder'=>"Nome"])!!} 
+            </label>
+            <label>
+               {!!Form::textarea ('descricao_pro',null,['class'=>'input', 'placeholder'=>"Descrição"])!!} 
+            </label>
+            <label>
+            {!! Form::number('quantidade_pro', null, ['class' => 'form-control', 'placeholder' => 'Quantidade']) !!}
+            </label>
+            <label>
                {!!Form::text('username',null,['class'=>'input', 'placeholder'=>"Usuario"])!!} 
             </label>
-        
+            <label>
+               {!!Form::text('username',null,['class'=>'input', 'placeholder'=>"Usuario"])!!} 
+            </label>
+            <label>
+               {!!Form::text('username',null,['class'=>'input', 'placeholder'=>"Usuario"])!!} 
+            </label>
+            <label>
+               {!!Form::text('username',null,['class'=>'input', 'placeholder'=>"Usuario"])!!} 
+            </label>
+            <label>
+               {!!Form::text('username',null,['class'=>'input', 'placeholder'=>"Usuario"])!!} 
+            </label>
+
+            {!! Form::number('number', null, ['class' => 'form-control', 'placeholder' => 'Números']) !!}
        
          {!!Form::close()!!}
    
