f6ce2a164c9c16db35277a02177cde52c2eaeb79
Richar2 <richard.coimec91@hotmail.com>
2019-02-01 20:06:50 -0200

    Atualização

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       // $this->call(ProdutoTableSeeder::class);
+       //Model::unguard();
+      
+       $this->call('produtosTableSeeder');
     }
 }
--- database/seeds/produtosTableSeeder.php
+++ database/seeds/produtosTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Produto;
+use  Faker\Factory as Faker;
 
 class produtosTableSeeder extends Seeder
 {
@@ -13,9 +14,16 @@ class produtosTableSeeder extends Seeder
     public function run()
     {
         DB::table('produtos')->truncate();
-        Produto::create([
-            'nome_pro'=>'prod 1',
-            'descricao_pro'=>'aqhu aqui'
-        ]);
+        
+        $faker= Faker::create();
+        
+        foreach(range(1,30) as $i) {
+          
+            Produto::create([
+                'nome_pro'=>$faker->word(),
+                'descricao_pro'=>$faker->sentence()
+            ]);
+        }
+
     }
 }
